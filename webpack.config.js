f08dfe0e7d78fca51f61afc385cc0e2416c4ea50
Alexey Khabirov <Khabirov.A@citilink.ru>
2020-12-01 00:26:02 +0300

    adds branch 4-1

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const TerserPlugin = require('terser-webpack-plugin');
 
 const isProd = process.argv.indexOf("-p") !== -1;
 
@@ -6,15 +7,20 @@ module.exports = {
   mode: isProd ? `production` : `development`,
   entry: `./src/main.js`,
   output: {
-    filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+    filename: 'bundle.js',
+    path: path.join(__dirname, `public`),
   },
   devtool: !isProd ? `inline-source-map` : false,
   devServer: {
     contentBase: path.join(__dirname, `public`),
     publicPath: `http://localhost:8080/`,
     compress: true,
-    watchContentBase: true
+    watchContentBase: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   module: {
     rules: [
@@ -23,5 +29,17 @@ module.exports = {
         use: [`style-loader`, `css-loader`],
       },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: isProd
+      ? [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false,
+        }),
+      ]
+      : [],
   },
 };
